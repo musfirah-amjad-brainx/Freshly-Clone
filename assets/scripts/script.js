a5f7be869520cc8cf037d587784d721e43cdda02
Musfirah <musfirah.amjad@brainxtech.com>
2022-11-04 10:44:37 +0500

    changes made in script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,12 +16,11 @@ window.onload = () => {
     // Add event listener on each meal card
     for (var i = 0; i < elements.length; i++) {
         elements[i].addEventListener('click', getMealID);
-        // elements[i].addEventListener('click', getMealID, false);
     }
 
     /* ----------------- Meal Plan Section End-----------------*/
 
-    /* ----------------- Date Section -----------------*/
+    /* ----------------- Date Section Start-----------------*/
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
@@ -86,16 +85,9 @@ window.onload = () => {
         })
 
     }
-
-    // }
     /* ----------------- Date Section End-----------------*/
 
     /* ----------------- Meals Cart Section Start-----------------*/
-
-    // let menu_template = document.getElementById("menu_template")
-    // console.log(menu_template);
-
-
     // Fetch Meal Menu
     fetch("assets/scripts/menu.json")
         .then(function (response) {
@@ -126,7 +118,7 @@ window.onload = () => {
     //initialize itemCount and subtotal
     var itemCount = 0;
     var subtotal = 0;
-    let continueBtn = document.querySelectorAll(".continueToCheckout");
+    var continueBtn = document.querySelectorAll(".continueToCheckout");
     continueBtn.disabled = true;
     function addToCart(productImg, title, description, additional_cost, cost, cart_img) {
 
@@ -151,37 +143,15 @@ window.onload = () => {
         document.getElementById("mealsInCart").appendChild(copyHTML);
 
         // Display Cart Item Count
-        itemCount = itemCount + 1
-        localStorage.setItem("itemCount", itemCount);
-        let counter = localStorage.getItem("itemCount")
-   
-        document.getElementById("itemCount").innerHTML = counter
-      
-        // ---Display remaining number of meals to be added in cart
-        let total_no_of_meals = localStorage.getItem("meal_plan");
-        let remaining = Number(total_no_of_meals) - Number(itemCount);
-        document.getElementById("remaining_items").innerHTML = remaining;
-
-        //Display total amount
-        subtotal = (Number(subtotal) + Number(cost) + Number(additional_cost)).toFixed(2);
-        localStorage.setItem("subtotal", subtotal);
-        let total = localStorage.getItem("subtotal")
-        document.getElementById("total_amount").innerHTML = total;
-
-        // Display Total Price in Checkout Section
-        document.getElementById("totelItems").innerHTML = itemCount;
-        document.getElementById("priceWithoutShipping").innerHTML = total;
-        totalWithShipping = 9.99 + Number(total)
-        document.getElementById("priceWithShipping").innerHTML = totalWithShipping;
-       
+        displayItemCount();
+        // Display Subtotal
+        displaySubTotal(cost, additional_cost);
+        // Display Final Order Details on Checkout
         finalOrder(title, itemCount, cart_img)
-  
-        if (itemCount == total_no_of_meals) {
-            continueBtn.disabled = false;
-        }
-
     }
+
     function removeMeal(e, cost, additional_cost, subtotal) {
+        console.log("Hello from remove Func")
         var element = e.target;
         element.parentElement.parentElement.parentElement.parentElement.remove();
 
@@ -196,122 +166,165 @@ window.onload = () => {
     function finalOrder(title, itemCount, cart_img) {
         var cartContent = document.getElementById("checkout_meals").content;
         var copyHTML = document.importNode(cartContent, true);
-        copyHTML.querySelector(".quantity").src = itemCount;
+        copyHTML.querySelector(".quantity").textContent = itemCount;
         copyHTML.querySelector(".checkoutImg").src = cart_img;
         copyHTML.querySelector(".checkout_title").textContent = title;
 
-        document.getElementById("my-meals").appendChild(copyHTML);
+        document.getElementById("my_meals").appendChild(copyHTML);
     }
+    // Function to increment and display meals
+    function displayItemCount() {
+        itemCount = itemCount + 1
+        localStorage.setItem("itemCount", itemCount);
+        let counter = localStorage.getItem("itemCount")
+        document.getElementById("itemCount").innerHTML = counter
+
+        // ---Display remaining number of meals to be added in cart
+        total_no_of_meals = localStorage.getItem("meal_plan");
+        let remaining = Number(total_no_of_meals) - Number(itemCount);
+        document.getElementById("remaining_items").innerHTML = remaining;
+
+        if (itemCount == total_no_of_meals) {
+            continueBtn.disabled = false;
+        }
+        return itemCount;
+    }
+    // Function to calculate and display total
+    function displaySubTotal(cost, additional_cost) {
+        //Display total amount
+        subtotal = (Number(subtotal) + Number(cost) + Number(additional_cost)).toFixed(2);
+        localStorage.setItem("subtotal", subtotal);
+        let total = localStorage.getItem("subtotal")
+        document.getElementById("total_amount").innerHTML = total;
+
+        // Display Total Price in Checkout Section
+        document.getElementById("totelItems").innerHTML = itemCount;
+        document.getElementById("priceWithoutShipping").innerHTML = total;
+        totalWithShipping = 9.99 + Number(total)
+        document.getElementById("priceWithShipping").innerHTML = totalWithShipping;
+    }
+
     /* ----------------- Meals Cart Section End-----------------*/
-    function verifyInputs() {
-        // -------------------Form Validation---------------
-        // getting form inputs
-        // const firstName = document.getElementById("fname");
-        // const lastName = document.getElementById("lname");
-        // const address1 = document.getElementById("address1");
-        // const city = document.getElementById("city");
-        // const contactNumber = document.getElementById("phone-num");
-        // const email = document.getElementById("email");
-        // const btn = document.getElementById("next-button");
 
-        // //adding event listener to call functions for validation
-        // firstName.addEventListener("focusout", validateFirstName)
-        // lastName.addEventListener("focusout", validateLastName)
-        // address1.addEventListener("focusout", validateAddress1)
-        // city.addEventListener("focusout", validateCity)
-        // contactNumber.addEventListener("focusout", validateContactNumber)
-        // email.addEventListener("focusout", validateEmail)
-        // btn.addEventListener("click", validateInput)
 
-        // // function to validate first name
-        // function validateFirstName() {
-        //     let _firstName = firstName.value;
-        //     if (_firstName.length <= 0) {
-        //         document.getElementById("fnameerrorMsg").innerHTML = "Can't be blank";
-        //     }
-        //     else {
-        //         document.getElementById("fnameerrorMsg").innerHTML = " ";
-        //         return true
-        //     }
-        // }
-        // // function to validate last name
-        // function validateLastName() {
-        //     let _lastName = lastName.value;
-        //     if (_lastName.length <= 0) {
-        //         document.getElementById("lnameerrorMsg").innerHTML = "Can't be blank";
-        //     }
-        //     else {
-        //         document.getElementById("lnameerrorMsg").innerHTML = " ";
-        //         return true
-        //     }
-        // }
-        // // function to validate address1
-        // function validateAddress1() {
-        //     let _address1 = address1.value;
-        //     // reg exp to validate address1
-        //     const addressRegEx = /^[a-zA-Z0-9\s,'-]*$/;
-        //     if (_address1.length <= 0 || !(_address1.match(addressRegEx))) {
-        //         document.getElementById("addresserrorMsg").innerHTML = "Can't be blank";
-        //     }
-        //     else {
-        //         document.getElementById("addresserrorMsg").innerHTML = " ";
-        //         return true
+    /*-----------------Checkout Section Start-------------*/
+    addPromoBtn=document.getElementById("addPromo");
+    addPromoBtn.addEventListener("click",function(e){
+        var NewInputBox = document.createElement('div');
 
-        //     }
-        // }
+    // Then add the content (a new input box) of the element.
+	NewInputBox.innerHTML = "<input type='text' id='newInputBox' class='form-control'> ";
 
-        // // function to validate citys
-        // function validateCity() {
-        //     let _city = city.value;
-        //     // creating array of multiple citys
-        //     if (_city.length <= 0) {
-        //         document.getElementById("cityerrorMsg").innerHTML = "Can't be blank";
-        //     }
-        //     else {
-        //         document.getElementById("cityerrorMsg").innerHTML = " ";
-        //         return true
-        //     }
-        // }
-        // // function to validate contact number
-        // function validateContactNumber() {
-        //     let _contactNumber = contactNumber.value;
-        //     if (_contactNumber.length < 10) {
-        //         document.getElementById("contacterrorMsg").innerHTML = "Is not a valid phone number. Please enter a 10-digit phone number.";
-        //     }
-        //     else {
+    // Finally put it where it is supposed to appear.
+	addPromoBtn.appendChild(NewInputBox);
+    })
 
-        //         document.getElementById("contacterrorMsg").innerHTML = " ";
-        //         return true
-        //     }
-        // }
-        // // function to validate email
-        // function validateEmail() {
-        //     let _email = email.value;
-        //     // reg exp to validate email
-        //     const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        //     if (_email.length <= 0 || !(_email.match(emailRegEx))) {
-        //         document.getElementById("emailerrorMsg").innerHTML = "Can't be blank";
-        //     }
-        //     else {
-        //         document.getElementById("emailerrorMsg").innerHTML = " ";
-        //         return true
+    /* ----------------- Form Validation Section start-----------------*/
+    // getting form inputs
+    const firstName = document.getElementById("fname");
+    const lastName = document.getElementById("lname");
+    const address1 = document.getElementById("address1");
+    const city = document.getElementById("city");
+    const contactNumber = document.getElementById("phone-num");
+    const email = document.getElementById("email");
+    const btn = document.getElementById("next-button");
 
-        //     }
+    //adding event listener to call functions for validation
+    firstName.addEventListener("focusout", validateFirstName)
+    lastName.addEventListener("focusout", validateLastName)
+    address1.addEventListener("focusout", validateAddress1)
+    city.addEventListener("focusout", validateCity)
+    contactNumber.addEventListener("focusout", validateContactNumber)
+    email.addEventListener("focusout", validateEmail)
+    btn.addEventListener("click", validateInput)
 
-        // }
-        // function validateInput() {
-        //     if (validateFirstName() && validateLastName() && validateCity() && validateAddress1() && validateEmail() && validateContactNumber()) {
-        //         alert("successfully submitted!")
-        //         document.getElementById("myForm").reset();
+    // function to validate first name
+    function validateFirstName() {
+        let _firstName = firstName.value;
+        if (_firstName.length <= 0) {
+            document.getElementById("fnameerrorMsg").innerHTML = "Can't be blank";
+        }
+        else {
+            document.getElementById("fnameerrorMsg").innerHTML = " ";
+            return true
+        }
+    }
+    // function to validate last name
+    function validateLastName() {
+        let _lastName = lastName.value;
+        if (_lastName.length <= 0) {
+            document.getElementById("lnameerrorMsg").innerHTML = "Can't be blank";
+        }
+        else {
+            document.getElementById("lnameerrorMsg").innerHTML = " ";
+            return true
+        }
+    }
+    // function to validate address
+    function validateAddress1() {
+        let _address1 = address1.value;
+        // reg exp to validate address
+        const addressRegEx = /^[a-zA-Z0-9\s,'-]*$/;
+        if (_address1.length <= 0 || !(_address1.match(addressRegEx))) {
+            document.getElementById("addresserrorMsg").innerHTML = "Can't be blank";
+        }
+        else {
+            document.getElementById("addresserrorMsg").innerHTML = " ";
+            return true
 
-        //     }
-        //     else {
-        //         // else display error
-        //         validateFirstName(); validateLastName(); validateCity(); validateAddress1(); validateEmail(); validateContactNumber();
-        //     }
-
-        // }
+        }
     }
 
-    //   -----------------Get Data from page-----------
+    // function to validate city
+    function validateCity() {
+        let _city = city.value;
+        if (_city.length <= 0) {
+            document.getElementById("cityerrorMsg").innerHTML = "Can't be blank";
+        }
+        else {
+            document.getElementById("cityerrorMsg").innerHTML = " ";
+            return true
+        }
+    }
+    // function to validate contact number
+    function validateContactNumber() {
+        let _contactNumber = contactNumber.value;
+        if (_contactNumber.length < 10) {
+            document.getElementById("contacterrorMsg").innerHTML = "Is not a valid phone number. Please enter a 10-digit phone number.";
+        }
+        else {
+
+            document.getElementById("contacterrorMsg").innerHTML = " ";
+            return true
+        }
+    }
+    // function to validate email
+    function validateEmail() {
+        let _email = email.value;
+        // reg exp to validate email
+        const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (_email.length <= 0 || !(_email.match(emailRegEx))) {
+            document.getElementById("emailerrorMsg").innerHTML = "Can't be blank";
+        }
+        else {
+            document.getElementById("emailerrorMsg").innerHTML = " ";
+            return true
+
+        }
+
+    }
+    function validateInput() {
+        if (validateFirstName() && validateLastName() && validateCity() && validateAddress1() && validateEmail() && validateContactNumber()) {
+            alert("successfully submitted!")
+            document.getElementById("myForm").reset();
+
+        }
+        else {
+            // else display error
+            validateFirstName(); validateLastName(); validateCity(); validateAddress1(); validateEmail(); validateContactNumber();
+        }
+
+    }
+    /* ----------------- Form Validation Section End-----------------*/
+
 }
